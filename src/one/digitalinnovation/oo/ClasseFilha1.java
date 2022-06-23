bdc7754c3a523dc73fccd0f3b7695edfc44164b6
rogeriodevelop <rogeriodevelop@gmail.com>
2022-06-22 21:59:36 -0300

    Polimorfismo e sobrescrita

---
+++ src/one/digitalinnovation/oo/ClasseFilha1.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.oo;
+
+public class ClasseFilha1 extends ClasseMae{
+
+    @Override
+    void metodo1() {
+        System.out.println("metodo 1 da Classe filha 1");
+    }
+}
---
+++ src/one/digitalinnovation/oo/ClasseMae.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.oo;
+
+public class ClasseMae {
+
+    void metodo1(){
+        System.out.println("metodo 1 da classe Mãe");
+    }
+
+    void metodo2(){
+        System.out.println("metodo 2 da Classe mãe");
+    }
+}
